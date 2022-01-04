3a0e30d3976df1af8d39fecc2755ed1db3c86043
Sergio <seres.crt@gmail.com>
2022-01-04 17:30:57 -0600

    readded coming soon text for search

--- pages/collection/search/index.tsx
+++ pages/collection/search/index.tsx
@@ -2,19 +2,21 @@ import { SearchName } from "../../../components/collection-search/name";
 export default function Search(){
 
     const submitHandler = (event:React.FormEvent) => {
-        
+
         //todo remove after testing 👇
         console.log('submit: ');
         //todo remove after testing 👆
     };
 
-    return (
+    /* return (
     <>
         <h1>Search Collection</h1>
         <form action="search/results/" onSubmit={submitHandler}>
             <SearchName />
             <input type="submit" value="Search"/>
         </form>  
-    </>
-    );
+    </> 
+    );*/
+
+    return (<h1>Coming soon...</h1>);
 }
